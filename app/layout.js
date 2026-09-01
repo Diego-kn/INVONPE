263407610d8c54ea4b2bde4ae02aacb65109b01f
Diego <penalozad815@gmail.com>
2026-09-01 16:38:45 -0500

    udpate icon global

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,17 @@ import "./globals.css";
 export const metadata = {
   title: 'Control de Indumentaria',
   description: 'Gestión de indumentaria e inventario',
+  icons: {
+    icon: '/favicon.ico', // O '/favicon.ico'
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/logo.png" sizes="any" />
+      </head>
       <body>{children}</body>
     </html>
   );
